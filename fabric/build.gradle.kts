dbad49542e19711237bfecf7e52f1baf1a9fe3e7
Toybox System <system@toybox.quest>
2025-06-21 11:35:16 +0100

    Remove fabric-loader from the fabric-api dependency

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -10,7 +10,6 @@ template {
             required()
 
             artifacts {
-                modImplementation(group = "net.fabricmc", name = "fabric-loader", version = Constants.FABRIC_LOADER_VERSION)
                 modImplementation(group = "net.fabricmc.fabric-api", name = "fabric-api", version = Constants.FABRIC_API_VERSION)
             }
         }
