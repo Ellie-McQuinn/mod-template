04b8dedb1176e867c4870a1b120e03957bf1b85e
Toybox System <system@toybox.quest>
2025-06-29 16:28:58 +0100

    Update FLK and KFF

--- buildSrc/src/main/kotlin/quest/toybox/template/Constants.kt
+++ buildSrc/src/main/kotlin/quest/toybox/template/Constants.kt
@@ -63,12 +63,12 @@ object Constants {
 
     // https://fabricmc.net/develop/
     const val FABRIC_API_VERSION = "0.116.2+1.21.1"
-    const val FABRIC_KOTLIN_VERSION = "1.13.2+kotlin.2.1.20"
-    const val FABRIC_LOADER_VERSION = "0.15.11"
+    const val FABRIC_KOTLIN_VERSION = "1.13.3+kotlin.2.1.21"
+    const val FABRIC_LOADER_VERSION = "0.16.9"
 
     const val NEOFORM_VERSION = "1.21.1-20240808.144430" // // https://projects.neoforged.net/neoforged/neoform/
     const val NEOFORGE_VERSION = "21.1.159" // https://projects.neoforged.net/neoforged/neoforge/
-    const val NEOFORGE_KOTLIN_VERSION = "5.8.0"
+    const val NEOFORGE_KOTLIN_VERSION = "5.9.0"
     const val FML_CONSTRAINT = "[4,)" // https://projects.neoforged.net/neoforged/fancymodloader/
 
     fun getProjectName(project: Project): String {
